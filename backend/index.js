e29b1d62a0398738b8b88ce043a94051519431b9
aldensd-1211 <aldendcunha1211@gmail.com>
2024-09-25 19:08:01 +0530

    add controllers

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-// import userRoute from "./routes/user.route.js";
+import userRoute from "./routes/user.route.js";
 // import companyRoute from "./routes/company.route.js";
 // import jobRoute from "./routes/job.route.js";
 // import applicationRoute from "./routes/application.route.js";
@@ -26,7 +26,7 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 // api's
-// app.use("/api/v1/user", userRoute);
+app.use("/api/v1/user", userRoute);
 // app.use("/api/v1/company", companyRoute);
 // app.use("/api/v1/job", jobRoute);
 // app.use("/api/v1/application", applicationRoute);
